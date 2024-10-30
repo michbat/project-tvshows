fc9330eab38fff9765f1b340c97ed17c8d5ca4d1
michbat <michael.batn@hotmail.com>
2024-10-30 02:39:12 +0100

    setting the API

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import s from "./style.module.css";
 import "./App.css";
+import { TVShowAPI } from "./api/tv-show";
+import { useState } from "react";
+import { useEffect } from "react";
+import { BACKDROP_BASE_URL } from "./config";
 
 function App() {
+  const [currentTVShow, setCurrentTVShow] = useState();
+
+  async function fetchPopulars() {
+    const populars = await TVShowAPI.fetchPopulars();
+    if (populars.length > 0) {
+      setCurrentTVShow(populars[0]);
+    }
+  }
+
+  // le hook useEffect se lancer au moins une fois (au niveau de chargement dans le DOM du component)
+  useEffect(() => {
+    fetchPopulars();
+  }, []);
+
+  console.log(currentTVShow);
   return (
-    <div className={s.main_container}>
+    <div
+      className={s.main_container}
+      style={{
+        background: currentTVShow
+          ? `linear-gradient(rgba(0,0,0,0.55), rgba(0,0,0,0.55)), url("${BACKDROP_BASE_URL}${currentTVShow.backdrop_path}") no-repeat center / cover`
+          : "black",
+      }}
+    >
       <div className={s.header}>
         <div className="row">
           <div className="col-4">
@@ -12,9 +38,8 @@ function App() {
             <div>Subtitle</div>
           </div>
           <div className="col-sm-12 col-md-4">
-            <input style={{ width: "100%" }} type="text"/>
+            <input style={{ width: "100%" }} type="text" />
           </div>
-
         </div>
       </div>
       <div className={s.tv_show_details}>Details</div>
---
+++ src/api/tv-show.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+import { BASE_URL, API_KEY_PARAM } from "../config";
+
+export class TVShowAPI {
+  static async fetchPopulars() {
+    const response = await axios.get(`${BASE_URL}tv/popular${API_KEY_PARAM}`);
+    return response.data.results;
+  }
+}
