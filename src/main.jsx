8da0342c3dc00762cff1efbe26a11dfebae4db44
michbat <michael.batn@hotmail.com>
2024-11-04 10:12:37 +0100

    final touch

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 // import { StrictMode } from 'react'
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { StrictMode } from "react";
 
-
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
